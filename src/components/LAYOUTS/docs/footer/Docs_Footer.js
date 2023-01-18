d309085b718d9424acd2f1a7c2a585dfa6598783
lucianrica <lucianrica@gmail.com>
2023-01-18 00:58:09 +0000

    .

--- src/components/LAYOUTS/docs/footer/Docs_Footer.js
+++ src/components/LAYOUTS/docs/footer/Docs_Footer.js
@@ -9,19 +9,22 @@ export default function Docs_Footer() {
         <footer className={styles.footer}>
             <div>
                 <h3>Did this doc help you?</h3>
+                <p>Promote good content by pressing like</p>
                 <a className={styles.btn}>Like</a>
                 <a href="#" className={`${styles.link} ${styles.small}`}>Privacy policy</a>
             </div>
             <div>
                 <h3>Help us make these docs great!</h3>
-                <p>All docs are enterprise wide open source. See something that's wrong or unclear?
-                    Submit a pull request.</p>
+                <p>All docs are inner source<sup style={{fontSize:'9px', fontWeight: '900' }} className={styles.sup}>T&C's</sup>. See something that's wrong or unclear?
+                    Submit a pull request</p>
                 <a className={styles.btn}> Make a contribution</a>
-                <a href="#" className={`${styles.link} ${styles.small}`}>Or, learn how to contribute.</a>
+                <a href="#" className={`${styles.link} ${styles.small}`}>Or, learn how to contribute</a>
             </div>
             <div>
                 <h3>Still need help?</h3>
-                <p>Contact x team for support</p>
+                <p>Contact x team on chat for support</p>
+                <h3>Urgent help?</h3>
+                <p>For any urgent queries, please raise incedent to {'dynamic-team'}</p>
             </div>
         </footer>
     )
